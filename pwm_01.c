325f45b89a0ee21fd86b24ae1f384b5cabd4cb92
Ricardo Prates <ricardo.prates@univasf.edu.br>
2025-02-05 10:16:02 -0300

    pronto para divulgar

--- pwm_01.c
+++ pwm_01.c
@@ -2,32 +2,24 @@
 #include "pico/stdlib.h" //subconjunto central de bibliotecas do SDK Pico
 #include "hardware/pwm.h" //biblioteca para controlar o hardware de PWM
 
-#define PWM_LED 12 //pino do LED conectado GPIO como PWM
-
+#define PWM_LED 16 //pino do LED conectado a GPIO como PWM
 
 //rotina principal
 int main()
 {
-    //inicializa o sistema padrão de I/O
-    stdio_init_all(); 
+    stdio_init_all(); //inicializa o sistema padrão de I/O
 
-    //habilitar o pino GPIO como PWM
-    gpio_set_function(PWM_LED, GPIO_FUNC_PWM);
+    gpio_set_function(PWM_LED, GPIO_FUNC_PWM); //habilitar o pino GPIO como PWM
 
-    //obter o canal PWM da GPIO
-    uint slice = pwm_gpio_to_slice_num(PWM_LED);
+    uint slice = pwm_gpio_to_slice_num(PWM_LED); //obter o canal PWM da GPIO
 
-    //define o divisor de clock do PWM
-    pwm_set_clkdiv(slice, 2.0);
+    pwm_set_clkdiv(slice, 2.0); //define o divisor de clock do PWM
 
-    //definir o valor de wrap
-    pwm_set_wrap(slice, 2000);
+    pwm_set_wrap(slice, 2000); //definir o valor de wrap
 
-    //definir o cico de trabalho (duty cycle) do pwm
-    pwm_set_gpio_level(PWM_LED, 1500);
+    pwm_set_gpio_level(PWM_LED, 500); //definir o ciclo de trabalho (duty cycle) do pwm
 
-    //habilita o pwm no slice correspondente
-    pwm_set_enabled(slice, true);
+    pwm_set_enabled(slice, true); //habilita o pwm no slice correspondente
 
     //loop principal
     while (true) {
